862205043b94e103bd6dd793e5bed693a33d592e
modar <modar.bayazed25@gmail.com>
2025-11-07 23:32:15 +0300

    add database Postqres

--- InternetApplications/Program.cs
+++ InternetApplications/Program.cs
@@ -1,23 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using InternetApplications.Data;
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString)
+);
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
