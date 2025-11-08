d8b725077184ee405c82f38558c6818918c05d55
modar <modar.bayazed25@gmail.com>
2025-11-08 19:09:38 +0300

    add swagger ui

--- InternetApplications/Program.cs
+++ InternetApplications/Program.cs
@@ -6,10 +6,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString)
 );
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 app.UseHttpsRedirection();
